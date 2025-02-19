d02e4931c624ad667811ebf9a98558cced6f0abd
angelnamuli <angelnamuli@icloud.com>
2025-02-19 20:03:26 +0000

    Add a simple code about limits

--- limits.py
+++ limits.py
@@ -1,1 +1,7 @@
-print("Hello world")
+print("Welcome to limits")
+
+def f(x):
+    return (x**2 - 1)/(x - 1) # function f(x) = (x^2 -1 )/(x - 1)
+x_values = [0.9,0.99,0.999,1.001,1.1] #values close to 1
+for x in x_values:
+    print(f"f({x}) = {f(x):.2f}")
